b6e30fd5fa636cf133191c447749aec77136a2d8
vAayush3301 <vaayush3301@gmail.com>
2025-10-12 15:24:49 +0530

    Drive API removed.

--- src/main/java/av/gdhns/club/main/controllers/UserController.java
+++ src/main/java/av/gdhns/club/main/controllers/UserController.java
@@ -1,26 +1,13 @@
 package av.gdhns.club.main.controllers;
 
 import av.gdhns.club.main.model.UserRegistrationModel;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.FileContent;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.drive.Drive;
-import com.google.api.services.drive.DriveScopes;
-import com.google.api.services.drive.model.File;
-import com.google.auth.http.HttpCredentialsAdapter;
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.ByteArrayInputStream;
-import java.util.Collections;
 
 @RestController
 @RequestMapping("/registration_member")
@@ -30,22 +17,8 @@ public class UserController {
     private final DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("member_registrations");
 
     @PostMapping
-    public ResponseEntity<String> createMemberRegistration(@RequestParam("data") String userData, @RequestParam("photo") MultipartFile photo, @RequestParam("video") MultipartFile video) {
+    public ResponseEntity<String> createMemberRegistration(@RequestBody UserRegistrationModel registration) {
         try {
-            UserRegistrationModel registration = new ObjectMapper().readValue(userData, UserRegistrationModel.class);
-            registration.setCreatedAt(java.time.Instant.now().toString());
-
-            java.io.File photoFile = java.io.File.createTempFile("photo", photo.getOriginalFilename());
-            photo.transferTo(photoFile);
-            String photoLink = uploadToDrive(photoFile, photo.getContentType());
-
-            java.io.File videoFile = java.io.File.createTempFile("video", video.getOriginalFilename());
-            video.transferTo(videoFile);
-            String videoLink = uploadToDrive(videoFile, video.getContentType());
-
-            registration.setPhotoLink(photoLink);
-            registration.setVideoLink(videoLink);
-
             System.out.println("Pushing registration: " + registration);
             userRef.child(registration.getPhoneNumber()).push().setValueAsync(registration).get();
 
@@ -53,30 +26,5 @@ public class UserController {
         } catch (Exception e) {
             return ResponseEntity.status(500).body("Error: " + e.getMessage());
         }
-    }
-
-    public Drive getDriveService() throws Exception {
-        GoogleCredentials credentials = GoogleCredentials.fromStream(new ByteArrayInputStream(driveJson.getBytes()))
-                .createScoped(Collections.singleton(DriveScopes.DRIVE_FILE));
-
-        return new Drive.Builder(GoogleNetHttpTransport.newTrustedTransport(),
-                JacksonFactory.getDefaultInstance(),
-                new HttpCredentialsAdapter(credentials)
-        ).setApplicationName("GGPS Club Project").build();
-    }
-
-    public String uploadToDrive(java.io.File localFile, String mimeType) throws Exception {
-        Drive service = getDriveService();
-
-        File fileMetadata = new File();
-        fileMetadata.setName(localFile.getName());
-
-        FileContent mediaContent = new FileContent(mimeType, localFile);
-
-        File uploadedFile = service.files().create(fileMetadata, mediaContent)
-                .setFields("id, webViewLink")
-                .execute();
-
-        return uploadedFile.getWebViewLink();
     }
 }
