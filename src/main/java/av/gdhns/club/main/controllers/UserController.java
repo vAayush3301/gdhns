22533d3df1c293d6f530e639845bb5652b40c45f
vAayush3301 <vaayush3301@gmail.com>
2025-10-12 15:47:03 +0530

    Resolved Duplicate Entries

--- src/main/java/av/gdhns/club/main/controllers/UserController.java
+++ src/main/java/av/gdhns/club/main/controllers/UserController.java
@@ -1,8 +1,7 @@
 package av.gdhns.club.main.controllers;
 
 import av.gdhns.club.main.model.UserRegistrationModel;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,11 +17,31 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<String> createMemberRegistration(@RequestBody UserRegistrationModel registration) {
+        final StringBuilder response = new StringBuilder();
+
         try {
+            userRef.orderByChild("phoneNumber").equalTo(registration.getPhoneNumber())
+                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.exists()) {
+                                response.append("User with phone ").append(registration.getPhoneNumber()).append(" already exists.");
+                            } else {
+                                userRef.child(registration.getPhoneNumber()).push().setValueAsync(registration);
+                                response.append("User added: ").append(registration.getFullName());
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                            response.append("Firebase error: ").append(databaseError.getMessage());
+                        }
+                    });
+
             System.out.println("Pushing registration: " + registration);
             userRef.child(registration.getPhoneNumber()).push().setValueAsync(registration).get();
 
-            return ResponseEntity.ok("New Member Registered: " + registration.getFullName());
+            return ResponseEntity.ok("Processing... Check logs for result");
         } catch (Exception e) {
             return ResponseEntity.status(500).body("Error: " + e.getMessage());
         }
